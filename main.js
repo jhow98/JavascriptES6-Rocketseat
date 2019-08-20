75750c6b21638a57e9548f9fafba246ae115e518
jhow98 <jhonatan@fho.edu.br>
2019-08-20 00:02:09 -0300

    Adicionando Herança

---
+++ main.js
@@ -0,0 +1,16 @@
+class List {
+    constructor(){
+        this.data = [];
+    }
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+class TodoList extends List{}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo');
+}

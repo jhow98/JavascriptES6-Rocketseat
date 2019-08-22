0801f42f791f0984d7982a22581dea12e9ca6c18
jhow98 <jhonatan@fho.edu.br>
2019-08-22 00:37:36 -0300

    Operadores rest/spread

---
+++ desestruturacao.js
@@ -0,0 +1,22 @@
+const usuario = {
+    nome: 'Diego',
+    idade: 23
+,
+endereco:{
+    cidade: "Rio do Sul",
+    estado: 'SC'
+},
+};
+
+//desestruturação
+
+const { nome, idade, endereco: { cidade } } = usuario;
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+function mostraNome({nome, idade}){
+    console.log(nome, idade);
+}
+mostraNome(usuario);

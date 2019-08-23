db3e77f80b84602112cc20e15e9db504551b55f1
jhow98 <jhonatan@fho.edu.br>
2019-08-22 22:26:16 -0300

    async await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-alert('testdddo');
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('ok') }, 2000);
+});
+
+const executaPromise = async () => {
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+}
+executaPromise();
